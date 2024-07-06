22292a5dd846ba1fdb6f85418d4657f1a56a1513
Fabo2303 <fabian666777@gmail.com>
2024-07-06 18:26:30 -0500

    cambio de html, css y js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-#SOPORTE TECNICO SO 
--- EN PROGRESO --
+# Página web - Capacitación de Debian
+
+Este proyecto tenía como objetivo la creación de una página web para la capacitación de Debian, pero no se llegó a
+completar.
+
+Lo usé como práctica para aprender a usar HTML y CSS.
+
+Se aplicó:
+
+- WebComponents
+- Importación de archivos JSON
--- script.js
+++ script.js
@@ -1,66 +1,54 @@
-var textos = ["completar-1", "completar-2", "completar-3", "completar-4",
-    "completar-5", "completar-6", "completar-7", "completar-8"];
+"use strict";
+import courses from './courses.json' with { type: 'json' };
 
-function toggleContenidos2(id, box_id) {
-    var elemento = document.getElementById(id);
-    var box = document.getElementById(box_id);
-    if (elemento.style.display === "none" || elemento.style.display === "") {
-        box.style.borderBottomLeftRadius = "0px";
-        box.style.borderBottomRightRadius = "0px";
-        elemento.style.display = "block";
-    } else {
-        elemento.style.display = "none";
-        box.style.borderBottomLeftRadius = "14px";
-        box.style.borderBottomRightRadius = "14px";
-    }
-}
+class CardComponent extends HTMLElement {
+    connectedCallback() {
+        const title = this.getAttribute('title') || 'Default Title';
+        const lessonCount = this.getAttribute('lesson-count') || 0;
+        const description = this.getAttribute('description') || 'Default Description';
+        let enableExpand = false;
 
-function toggleComplete(id) {
-    var myTexto = document.getElementById(id);
-    myTexto.textContent = "1/1 completado";
-    myTexto.style.color = "blue";
-    var i = 0;
-    for (const texto of textos) {
-        var compl = document.getElementById(texto).textContent;
-        if (compl == "1/1 completado") {
-            i++;
-        }
-        console.log(i);
-    }
-    if (i == 8) {
-        changeDisplay();
-    }
-}
+        this.innerHTML = `
+        <div class="course-container">
+            <section class="course-card">
+                <div>
+                    <h3>${title}</h3>
+                    <h5>0/${lessonCount} Completado</h5>
+                </div>
+                <span class="material-symbols-outlined" id="expand-icon">
+                    expand_more
+                </span>
+            </section>
+            <section class="course-description" id="course-description" style="display: none">
+                <p>${description}</p>
+            </section>
+        </div>
+        `;
 
-function changeDisplay() {
-    var boton = document.getElementById('certificado');
-    boton.style.display = "block";
-}
+        const expandIcon = this.querySelector('#expand-icon');
+        const courseDescription = this.querySelector('#course-description');
 
-function toggleDatos(id) {
-    var box = document.getElementById(id);
-    box.style.display = "flex";
-}
-
-function firmar(id, id2, id3) {
-    var textField1 = document.getElementById(id).value;
-    var textField2 = document.getElementById(id2).value;
-    var textField3 = document.getElementById(id3).value;
-    if (textField1 != "" && textField2 != "" && textField3 != "") {
-        Swal.fire({
-            title: "¡Felicidades!",
-            html: textField1 + " " + textField2 + ", felicidades por completar tu capacitación con éxito " +
-                "y obtener tu merecido certificado. Te deseamos un futuro profesional lleno de éxitos." + 
-                "<br>Atentamente, SecureShop Peru",
-            icon: "success",
-            confirmButtonText: "Aceptar",
-        });
-    } else {
-        Swal.fire({
-            title: "¡ERROR!",
-            html: "Llena los espacios en blanco",
-            icon: "error",
-            confirmButtonText: "Aceptar",
+        expandIcon.addEventListener('click', () => {
+            enableExpand = !enableExpand;
+            if (enableExpand) {
+                courseDescription.style.display = 'flex';
+            } else {
+                courseDescription.style.display = 'none';
+            }
         });
     }
 }
+
+function showCards(){
+    const coursesContainer = document.getElementById('main-container');
+    courses.forEach(course => {
+        const card = document.createElement('card-component');
+        card.setAttribute('title', course.title);
+        card.setAttribute('lesson-count', course.lessons.length);
+        card.setAttribute('description', course.description);
+        coursesContainer.appendChild(card);
+    });
+}
+
+customElements.define('card-component', CardComponent);
+showCards();
